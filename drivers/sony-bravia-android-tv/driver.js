62609a98f7e2a036da90327c8d0b55a7ef98c3bb
Ricardo Snoek <me@ricardoismy.name>
2023-01-15 23:19:11 +0100

    Rewriting pairing driver to follow new SDK3 format

--- drivers/sony-bravia-android-tv/driver.js
+++ drivers/sony-bravia-android-tv/driver.js
@@ -3,48 +3,59 @@ const Homey = require('homey');
 const SonyBraviaAndroidTvFinder = require('../../helpers/sony-bravia-android-tv-finder');
 
 class SonyBraviaAndroidTvDriver extends Homey.Driver {
-  onPair(socket) {
-    socket.on('list_devices', (_devices, callback) => this.fetchAvailableDevices(callback, socket));
-    socket.on('manual_input', (data, callback) => this.fetchDeviceDetails(data, callback));
-    socket.on('preshared_key', (device, callback) => this.fetchExpandedDeviceDetails(device, callback));
+  onPair(session) {
+    session.setHandler('list_devices', async () => await this.fetchAvailableDevices(session));
+    session.setHandler('manual_input', async (data) => await this.fetchDeviceDetails(data));
+    session.setHandler('preshared_key', async (device) => await this.fetchExpandedDeviceDetails(device));
   }
 
   async fetchExpandedDeviceDetails(device, callback) {
     try {
-      const extendedDevice = await SonyBraviaAndroidTvFinder.fetchExtendDeviceDetails(device);
+      const extendedDevice =
+        await SonyBraviaAndroidTvFinder.fetchExtendDeviceDetails(device);
 
-      console.log('Got extended Sony BRAVIA Android TV data: ', extendedDevice);
+      this.log('Got extended Sony BRAVIA Android TV data: ', extendedDevice);
 
-      callback(null, extendedDevice);
+      return extendedDevice;
     } catch (err) {
-      callback(err, null);
+      this.err(err);
     }
+
+    return null;
   }
 
-  async fetchDeviceDetails(data, callback) {
-    const device = SonyBraviaAndroidTvFinder.populateDeviceData(data.name, null, data.ipAddress, data.macAddress);
+  async fetchDeviceDetails(data) {
+    const device = SonyBraviaAndroidTvFinder.populateDeviceData(
+      data.name,
+      null,
+      data.ipAddress,
+      data.macAddress
+    );
 
     try {
-      const basicDevice = await SonyBraviaAndroidTvFinder.fetchBasicDeviceDetails(device);
+      const basicDevice =
+        await SonyBraviaAndroidTvFinder.fetchBasicDeviceDetails(device);
 
-      console.log('Got basic Sony BRAVIA Android TV data: ', basicDevice);
+      this.log('Got basic Sony BRAVIA Android TV data: ', basicDevice);
 
-      callback(null, basicDevice);
+      return basicDevice;
     } catch (err) {
-      callback(err, null);
+      this.err(err);
     }
+
+    return null;
   }
 
-  async fetchAvailableDevices(callback, socket) {
+  async fetchAvailableDevices(session) {
     const devices = await SonyBraviaAndroidTvFinder.getAllDevices();
 
-    console.log('Found Sony BRAVIA Android TV\'s: ', devices);
+    this.log('Found Sony BRAVIA Android TV\'s: ', devices);
 
     if (devices.length < 1) {
-      socket.showView('not_found');
+      session.showView('not_found');
     }
 
-    callback(null, devices);
+    return devices;
   }
 }
 
